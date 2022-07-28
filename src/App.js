5d9f8fc3800070c441ee59d1990d0f4e7e6c79ba
Prafull Epili <prafullepili@gmail.com>
2022-07-28 10:19:23 +0530

    Organizing Components files to Expenses and UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import Card from './Card.js';
+import Card from '../UI/Card.js';
 import ExpenseItem from './ExpenseItem.js';
 
 import "./Expenses.css"
