ee9ddc4cd45e09837b3cc6c411d9b11da9b8a6e5
Prafull Epili <prafullepili@gmail.com>
2022-07-28 09:38:55 +0530

    added Expenses.js/css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
-import './index'
+import './index';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -12,10 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <ExpenseItem key={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem key={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem key={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem key={expenses[3].id} title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <Expenses items={expenses} />
     </div>
   );
 }
